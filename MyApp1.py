3a829a66ce580fe3b642b3e9a097551f0702434b
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-03-28 10:54:19 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Website Developing using Python")
 st.header("Website Developing using Python")
 st.subheader("Kairung Hengpraprohm")
-st.img('')
+st.img('kairung.jpg')

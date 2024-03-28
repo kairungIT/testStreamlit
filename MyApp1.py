1faff558a56bfa7ab173c45040b769b36adf195f
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-03-28 12:03:23 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -21,10 +21,11 @@ cl1.write(dt['sepal.length'].sum())
 cl2.write(dt['sepal.width'].sum())
 cl3.write(dt['petal.length'].sum())
 cl4.write(dt['petal.width'].sum())
+
 dx=pd.DataFrame(
     {
-     "col1":dt['sepal.length'].sum()
-     "col2":dt['sepal.width'].sum()
+     "col1":dt['sepal.length'].sum(),
+     "col2":dt['sepal.width'].sum(),
      "col3":dt['petal.length'].sum()
     }
 )

bd5de3cbf5d2dbb4eee6382818f1184319c01dd8
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-03-28 11:26:45 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🍌🍌Website Developing using Python🍌🍌")
 st.header("🍖🍖Website Developing using Python🍖🍖")
 
 st.subheader("Kairung Hengpraprohm")
 st.image('kairung.jpg')
+
+dt=pd.read_csv('./data/iris.csv')
+st.write(dt.head(10))

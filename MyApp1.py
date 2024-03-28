d6748c58bc71216917dc1dfaac74c09981257320
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-03-28 12:36:14 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -22,12 +22,13 @@ cl2.write(dt['sepal.width'].sum())
 cl3.write(dt['petal.length'].sum())
 cl4.write(dt['petal.width'].sum())
 
-st.write("xxx")
+st.write("กราฟแท่ง")
 a=dt['sepal.length'].sum()
 b=dt['sepal.width'].sum()
 c=dt['petal.length'].sum()
-dx=[a,b,c]
-cx=pd.DataFrame(dx,index=["sepal.length", "sepal.width", "petal.length"])
+d=dt['petal.width'].sum()
+dx=[a,b,c,d]
+cx=pd.DataFrame(dx,index=["sepal.length", "sepal.width", "petal.length","petal.width"])
 st.bar_chart(cx)
 
 st.write('ค่าเฉลี่ย')
@@ -37,6 +38,16 @@ cl12.write(dt['sepal.width'].mean())
 cl13.write(dt['petal.length'].mean())
 cl14.write(dt['petal.width'].mean())
 
+st.write("Area_Chart")
+a=dt['sepal.length'].mean()
+b=dt['sepal.width'].mean()
+c=dt['petal.length'].mean()
+d=dt['petal.width'].mean()
+dx=[a,b,c,d]
+cx=pd.DataFrame(dx,index=["sepal.length", "sepal.width", "petal.length","petal.width"])
+st.bar_chart(cx)
+
+
 st.write('ค่ามากที่สุด')
 cl21,cl22,cl23,cl24=st.columns(4)
 cl21.write(dt['sepal.length'].max())
@@ -44,9 +55,34 @@ cl22.write(dt['sepal.width'].max())
 cl23.write(dt['petal.length'].max())
 cl24.write(dt['petal.width'].max())
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.write("Pie_Chart")
+a=dt['sepal.length'].max()
+b=dt['sepal.width'].max()
+c=dt['petal.length'].max()
+d=dt['petal.width'].max()
+dx=np.array[a,b,c,d]
+labels=["sepal.length", "sepal.width", "petal.length","petal.width"]
+myexplode=[0.2,0,0,0]
+
+plt.pie(dx,labels=labels,explode=myexplode)
+plt.show()
+
 st.write('ค่าน้อยที่สุด')
 cl31,cl32,cl33,cl34=st.columns(4)
 cl31.write(dt['sepal.length'].min())
 cl32.write(dt['sepal.width'].min())
 cl33.write(dt['petal.length'].min())
 cl34.write(dt['petal.width'].min())
+
+st.write("Line_Chart")
+a=dt['sepal.length'].min()
+b=dt['sepal.width'].min()
+c=dt['petal.length'].min()
+d=dt['petal.width'].min()
+dx=np.array[a,b,c,d]
+
+plt.plot(dx,'o:r',ms=10)
+plt.show()
